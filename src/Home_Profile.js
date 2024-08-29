db7501bf603475b2731a94267f02cbdf56a41e3d
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2024-08-30 01:20:18 +0400

    02

--- src/Home_Profile.js
+++ src/Home_Profile.js
@@ -200,7 +200,7 @@ function HomeProfile() {
       <HeroContent>
         <Container maxWidth="sm">
           <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Welcome to Reflection
+            ზდაროვია
           </Typography>
           <Typography variant="h5" align="center" color="textSecondary" paragraph>
             Share your personal notes, thoughts, and experiences at specific locations. Connect with others through shared memories and insights.
