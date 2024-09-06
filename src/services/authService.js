45359901b8d3dd92589f7650ef3219527dac8e5b
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2024-09-06 20:55:49 +0400

    86

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8081';
-const API_URL = 'https://api.brooks-dusura.uk';
+const API_URL = 'https://apii.brooks-dusura.uk';
 
 const login = (credentials) => {
   return axios.post(`${API_URL}/user-auth/login/login`, credentials)
